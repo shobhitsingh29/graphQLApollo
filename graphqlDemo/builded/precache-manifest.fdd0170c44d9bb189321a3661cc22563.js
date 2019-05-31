1caa3ee1615706cda67da1d27b83a65f6e437ca8
shobhit.singh <shobhit.singh@flipkart.com>
2019-05-31 22:50:19 +0530

    init

---
+++ graphqlDemo/builded/precache-manifest.fdd0170c44d9bb189321a3661cc22563.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70074842d85d847f664866c46e5c5f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ed3db0138b1028998b6",
+    "url": "/static/css/main.8d733da0.chunk.css"
+  },
+  {
+    "revision": "31f4588b89dc6474e0d3",
+    "url": "/static/js/2.58c188bc.chunk.js"
+  },
+  {
+    "revision": "0ed3db0138b1028998b6",
+    "url": "/static/js/main.bed7a94b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
